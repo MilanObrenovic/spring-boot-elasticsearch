88fa0f17d5d149edc1af7ae1db20a6c7ee99576e
Milan Obrenovic <mmobrenovic@gmail.com>
2024-02-27 18:41:08 +0100

    Added repository

---
+++ src/main/java/org/example/demo/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package org.example.demo.repository;
+
+import org.example.demo.model.CarModel;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface CarRepository extends ElasticsearchRepository<CarModel, String> {
+
+
+
+}
